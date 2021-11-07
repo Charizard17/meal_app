8d0660433e7c8973343e908a8c27404a4686ab5e
Esad Dursun <3723esad@gmail.com>
2021-11-07 11:49:56 +0100

    category model created. categories screen widget created. dummy category data created

---
+++ lib/dummy_data.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: 'Italian', color: Colors.blue),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c4', title: 'German', color: Colors.pink),
+  Category(id: 'c5', title: 'Exotic', color: Colors.green),
+  Category(id: 'c6', title: 'Breakfast', color: Colors.lightBlue),
+  Category(id: 'c7', title: 'Asian', color: Colors.lightGreen),
+  Category(id: 'c8', title: 'French', color: Colors.brown),
+  Category(id: 'c9', title: 'Summer', color: Colors.teal),
+  Category(id: 'c10', title: 'Light & Lovely', color: Colors.cyan),
+];
